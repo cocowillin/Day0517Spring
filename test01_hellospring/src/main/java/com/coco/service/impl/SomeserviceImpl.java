4351584ab432102a94f7fd7cae084514bb4d555a
cocolin <smartxiaow@gmail.com>
2021-05-17 22:37:30 +0800

    2021年5月17日03:31:24

--- test01_hellospring/src/main/java/com/coco/service/impl/SomeserviceImpl.java
+++ test01_hellospring/src/main/java/com/coco/service/impl/SomeserviceImpl.java
@@ -10,5 +10,7 @@ public class SomeserviceImpl implements Someservice {
     @Override
     public void doSome() {
         System.out.println("执行了someservice的doSome方法！");
+        System.out.println("修改 2021年5月17日22:36:40 ");
+
     }
 }
