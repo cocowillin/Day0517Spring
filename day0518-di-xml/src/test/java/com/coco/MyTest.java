04404136f54d6c472958baf313c0b6aa0f34b819
cocolin <smartxiaow@gmail.com>
2021-05-18 22:45:09 +0800

    时间 属性的赋值 di注入

--- day0518-di-xml/src/test/java/com/coco/MyTest.java
+++ day0518-di-xml/src/test/java/com/coco/MyTest.java
@@ -1,5 +1,6 @@
 package com.coco;
 
+import com.coco.ba01.Student;
 import com.coco.service.Someservice;
 import com.coco.service.impl.SomeserviceImpl;
 import org.junit.Test;
@@ -13,41 +14,14 @@ import java.util.Date;
  * @create 2021-05-17 3:20
  */
 public class MyTest {
-    @Test /*主动方式创建对象和调用方法*/
-    public void test01(){
-        Someservice someservice = new SomeserviceImpl();
-        someservice.doSome();
-    }
+
     @Test
-    public void test02(){
-        String config="beans.xml";
-        ApplicationContext ac=new ClassPathXmlApplicationContext(config);
-        /*Someservice someService = (Someservice) ac.getBean("someService");
-        Someservice someService1 = (Someservice) ac.getBean("someService1");
-        someService.doSome();
-        someService1.doSome();
-*/
-        /*验证 读取配置文件时候就创建了对象 无参数构造方法执行了*/
-
-        int nums = ac.getBeanDefinitionCount();
-        //获取容器中定义对象的数量
-        System.out.println("容器中定义对象的数量是   "+nums);
-
-        //容器中定义对象的名称
-        System.out.println("***********************");
-        String[] names = ac.getBeanDefinitionNames();
-        for(String name:names){
-            System.out.println(name);
-        }
-
+    public void test01(){
+        String config = "ba01/applicationContext.xml";
+        ApplicationContext ac = new ClassPathXmlApplicationContext(config);
+        Student myStudent = (Student) ac.getBean("myStudent");
+        System.out.println(myStudent);
 
     }
-    @Test//创建不是自己的对象 系统的对象 已经创建好的 或者别人的对象
-    public void test03(){
-        String config="beans.xml";
-        ApplicationContext ac=new ClassPathXmlApplicationContext(config);
 
-        Date myDate = (Date) ac.getBean("myDate");
-        System.out.println(myDate);
-    }
 }
